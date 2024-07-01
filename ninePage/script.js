d82602b1377e881123a0e77ab01c302a5e3ce572
Wisdom-Kunambi <kunambiwaziri1@gmail.com>
2024-07-01 18:31:26 +0300

    add changes in the ninePage folder

--- ninePage/script.js
+++ ninePage/script.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         document.getElementById('loading-screen').style.display = 'none';
         document.getElementById('app').style.display = 'block';
-    }, 3000); // Change the duration as needed
+    }, 3000); 
 });
